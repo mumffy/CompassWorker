ce44384beba4d93cd01720496cf1a2299e22205f
mumffy <mumffy@gmail.com>
2017-08-06 14:53:20 -0700

    test updates

--- compassworker/scraper.py
+++ compassworker/scraper.py
@@ -3,25 +3,34 @@ from bs4 import BeautifulSoup
 
 
 class CompassCard(object):
+    Requests = requests
+
     _url_frontpage = 'https://compasscard.511sd.com'
     _url_login = 'https://compasscard.511sd.com/webtix/welcome/welcome.do'
 
     def __init__(self, card_number, password):
         self._card_number = card_number
         self._password = password
+        self._balance = None
+        self._balance_update_date = None
         self._login_get_balance()
 
     def _login_get_balance(self):
         post_payload = {
-            'cardNum':self._card_number,
-            'pass':self._password,
-            'cardOps':'Display',
+            'cardNum': self._card_number,
+            'pass': self._password,
+            'cardOps': 'Display',
         }
-        response = requests.post(self._url_login, post_payload)
-        soup = BeautifulSoup(response.text, 'html5lib')
-        balance_update_date, balance = [x.get_text() for x in soup.find('table', class_='results_table', id=False).find_all("td")]
-        self._balance = float(balance.replace('$',''))
-        self._balance_update_date = balance_update_date
+        try:
+            response = self.Requests.post(self._url_login, post_payload)
+            soup = BeautifulSoup(response.text, 'html5lib')
+        except:
+            pass
+        else:
+            balance_update_date, balance = [x.get_text() for x in
+                                            soup.find('table', class_='results_table', id=False).find_all("td")]
+            self._balance = float(balance.replace('$', ''))
+            self._balance_update_date = balance_update_date
 
     @property
     def card_number(self):
@@ -30,3 +39,7 @@ class CompassCard(object):
     @property
     def balance(self):
         return self._balance
+
+    @property
+    def balance_update_date(self):
+        return self._balance_update_date
--- tests/functional/test_scraper.py
+++ tests/functional/test_scraper.py
@@ -4,8 +4,34 @@ from compassworker.scraper import CompassCard
 from tests import test_config
 
 
+class FakeResponse(object):
+    def __init__(self, text):
+        self.text = text
+
+
+class MockRequests(object):
+    #TODO inject test_config.compass_cards data into MockRequests instance
+
+    @staticmethod
+    def post(url, post_payload_dict):
+        for card in test_config.compass_cards:
+            if card.number == post_payload_dict['cardNum']:
+                return FakeResponse(card.balance_page_html)
+        return FakeResponse("crap")
+
+
 class TestCompassCard(TestCase):
-    def test_balance(self):
+    @classmethod
+    def setUpClass(cls):
+        CompassCard.Requests = MockRequests
+        pass
+
+    def test_balance01(self):
         card_config = test_config.compass_cards[0]
         cc = CompassCard(card_config.number, card_config.password )
         self.assertEqual(cc.balance, 12.50)
+
+    def test_balance02(self):
+        card_config = test_config.compass_cards[1]
+        cc = CompassCard(card_config.number, card_config.password)
+        self.assertEqual(cc.balance, 8.00)
